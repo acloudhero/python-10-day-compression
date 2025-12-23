487825d170d5cf420c854a9e00b3bbd34e4d4287
EB <ericbharrisii@gmail.com>
2025-12-23 12:27:06 -0600

    Day 3: Loops and iteration boundaries

---
+++ day-03-loops/loops.py
@@ -0,0 +1,23 @@
+# Day 3: Loops and itteration boundries
+
+# print("Counting from 1 to 5")
+
+# count = 1
+
+# while count <= 5:
+  #  print("count is:", count)
+   # count += 1
+
+# print("Loop complete")
+
+
+# next step add filering
+
+print("Print even numbers between 1 and 10")
+
+number = 1
+
+while number <= 10:
+    if number % 2 == 0:
+        print("even number:", number)
+    number += 1
